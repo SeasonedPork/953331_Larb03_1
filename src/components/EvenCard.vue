3e28f6224d7e9ffce7a4fe15e150bc04f1692fd5
Yanawarut Kanwana <yanawarood.yk@gmail.com>
2022-07-21 17:55:44 +0700

    init

---
+++ src/components/EvenCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="event-card">
+    <span>@{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  Props: {
+    events: [
+      {
+        id: 5928101,
+        category: "animal welfare",
+        title: "Cat Adoption Day",
+        description: "Find your new feline friend at this event.",
+        location: "Meow Town",
+        date: "January 28, 2022",
+        time: "12:00",
+        petsAllowed: true,
+        organizer: "Kat Laydee",
+      },
+      {
+        id: 4582797,
+        category: "food",
+        title: "Community Gardening",
+        description: "Join us as we tend to the community edible plants.",
+        location: "Flora City",
+        date: "March 14, 2022",
+        time: "10:00",
+        petsAllowed: true,
+        organizer: "Fern Pollin",
+      },
+      {
+        id: 8419988,
+        category: "sustainability",
+        title: "Beach Cleanup",
+        description: "Help pick up trash along the shore.",
+        location: "Playa Del Carmen",
+        date: "July 22, 2022",
+        time: "11:00",
+        petsAllowed: false,
+        organizer: "Carey Wales",
+      },
+    ],
+  },
+};
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
